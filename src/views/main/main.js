277851c3e5f9f99a163150c53b999444af82d078
heejung <cheej10@naver.com>
2022-05-25 03:32:48 +0900

    목록, 상세페이지 레이아웃 작성

---
+++ src/views/main/main.js
@@ -0,0 +1,28 @@
+// 2) Image Slider
+
+var slider = document.querySelector('.slider');
+var slides = document.querySelector('.slides');
+var slide = document.querySelectorAll('.slide');
+
+var currentSlide = 0;
+
+setInterval(function () {
+  var from = -(960 * currentSlide);
+  var to = from - 960;
+  slides.animate(
+    {
+      marginLeft: [from + 'px', to + 'px'],
+    },
+    {
+      duration: 500,
+      easing: 'ease',
+      iterations: 1,
+      fill: 'both',
+    }
+  );
+  currentSlide++;
+  if (currentSlide === slide.length - 1) {
+    currentSlide = -1;
+  }
+  console.log(currentSlide, slide.length);
+}, 3000);
