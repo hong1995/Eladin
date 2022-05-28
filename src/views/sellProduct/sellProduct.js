0ebd4e3e41b287873db882f9332a800adb78a95c
heejung <cheej10@naver.com>
2022-05-28 11:26:22 +0900

    sellProduct 수정

--- src/views/sellProduct/sellProduct.js
+++ src/views/sellProduct/sellProduct.js
@@ -9,27 +9,25 @@ const photo = document.querySelector('#photo');
 const priceInput = document.querySelector('#price');
 const purchaseButton = document.querySelector('#purchaseButton');
 
-
 purchaseButton.addEventListener('click', sell);
 
-
 async function sell(e) {
-    e.preventDefault();
+  e.preventDefault();
 
-    const bookName = bookNameInput.value;
-    const author = authorInput.value;
-    const category = categoryInput.value;
-    const publisher = publisherInput.value;
-    const info = infoInput.value;
-    const price = priceInput.value;
+  const bookName = bookNameInput.value;
+  const author = authorInput.value;
+  const category = categoryInput.value;
+  const publisher = publisherInput.value;
+  const info = infoInput.value;
+  const price = Number(priceInput.value);
 
-    try {
-        console.log(price)
-        const data = {bookName, author, category, publisher, price, info }
-        const result = await Api.post('/product/register',data);
-        console.log(result);
-    } catch (e) {
-        console.error(err.stack);
-        alert(`문제가 발생하였습니다. 확인 후 다시 시도해 주세요: ${err.message}`);
-    }
+  try {
+    console.log(price);
+    const data = { bookName, author, category, publisher, price, info };
+    const result = await Api.post('/product/register', data);
+    console.log(result);
+  } catch (e) {
+    console.error(err.stack);
+    alert(`문제가 발생하였습니다. 확인 후 다시 시도해 주세요: ${err.message}`);
+  }
 }
