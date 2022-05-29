f69a3e5e8332e807a1a63b56e4ac6063095fd61c
wldn143 <wldn143@gmail.com>
2022-05-29 19:19:42 +0900

    validator 추가

---
+++ src/middlewares/validator.js
@@ -0,0 +1,11 @@
+const { validationResult } = require("express-validator");
+//회원가입시 유효성 검사 에러 보내주는 역할
+module.exports = {
+  validateError: (req, res, next) => {
+    const errors = validationResult(req);
+    if (errors.isEmpty()) {
+      return next();
+    }
+    return res.status(400).json({ message: errors.array()[0].msg });
+  },
+};
