a1a1c3bef87da57624f45cdf88e4ff923d0a8ef0
heejung <cheej10@naver.com>
2022-06-01 02:02:45 +0900

    사용자 페이지에 navbar 추가

--- src/views/navBar/navBar.js
+++ src/views/navBar/navBar.js
@@ -6,8 +6,6 @@ const loginNavBar = `<nav class="navbar">
   <ul class="navbar-user">
     <li><a href="/register">회원가입</a></li>
     <li><a href="/login">로그인</a></li>
-    <li><a href="/accountManagement">계정관리</a></li>
-    <li><a href="/cart">장바구니</a></li>
   </ul>
   </nav>`;
 
@@ -17,14 +15,15 @@ const logoutNavBar = `<nav class="navbar">
   <li><a href="/list">도서목록</a></li>
 </ul>
 <ul class="navbar-user">
-  <li><a href="/register">회원가입</a></li>
   <li><a href = "/" id = 'logout'>로그아웃</a></li>
   <li><a href="/accountManagement">계정관리</a></li>
   <li><a href="/cart">장바구니</a></li>
 </ul>
 </nav>`;
 
+// 화면에 부착
 const e = document.createElement('div');
+
 if (localStorage.getItem('token')) {
   e.innerHTML = logoutNavBar;
 } else {
@@ -33,6 +32,7 @@ if (localStorage.getItem('token')) {
 
 document.body.insertBefore(e, document.body.firstChild);
 
+// 로그아웃
 const out = document.querySelector('#logout');
 out.addEventListener('click', logout);
 
