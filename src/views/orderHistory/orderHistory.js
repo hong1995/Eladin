96692c9c6516e1ca0d06325094f8e6779015b919
cheej10 <cheej10@naver.com>
2022-06-01 18:31:10 +0900

    front merge

--- src/views/orderHistory/orderHistory.js
+++ src/views/orderHistory/orderHistory.js
@@ -1,24 +1,26 @@
 import * as Api from '../api.js';
 
-const ordersItemContainer = document.querySelector('.orders-item-container');
+const contentContainer = document.querySelector('.content-container');
 
 const orders = await Api.get('/order/orders');
 
 orders.forEach((order) => {
   const dateText = order.createdAt.split('T')[0];
-  let nameText;
-  let quantityText;
+  let element = null;
 
-  order.orderList.forEach((order) => {
-    const { bookName, quantity } = order;
+  // 한 주문에 상품 여러개일 때
+  if (order.orderList.length > 1) {
+    const nameText = order.orderList[0].bookName;
+    const quantityText = order.orderList.reduce(
+      (acc, cur) => acc + cur.quantity,
+      0
+    );
+    const remainsCount = order.orderList.length - 1;
 
-    nameText = bookName;
-    quantityText = String(quantity);
-
-    const element = `
+    element = `
     <div class="order-item">
         <div class="item-date">${dateText}</div>
-        <div class="item-book">${nameText}</div>
+        <div class="item-book">${nameText} 외 ${remainsCount}건</div>
         <div class="item-quantity">${quantityText}</div>
         <div class="item-status">상품 준비중</div>
         <div class="item-delete-request">
@@ -26,14 +28,28 @@ orders.forEach((order) => {
         </div>
     </div>
   `;
+  } else {
+    const { bookName, quantity } = order.orderList[0];
 
-    ordersItemContainer.insertAdjacentHTML('beforeend', element);
-  });
+    element = `
+    <div class="order-item">
+        <div class="item-date">${dateText}</div>
+        <div class="item-book">${bookName}</div>
+        <div class="item-quantity">${String(quantity)}</div>
+        <div class="item-status">상품 준비중</div>
+        <div class="item-delete-request">
+            <div class="delete-btn">주문 취소</div>
+        </div>
+    </div>
+  `;
+  }
+
+  contentContainer.insertAdjacentHTML('beforeend', element);
 });
 
 // 주문 취소
-const orderItem = document.querySelectorAll('.order-item');
 const deleteBtn = document.querySelectorAll('.delete-btn');
+const orderItem = document.querySelectorAll('.order-item');
 
 deleteBtn.forEach((node, i) => {
   node.addEventListener('click', async () => {
